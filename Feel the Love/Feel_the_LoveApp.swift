2261c58574ffeada0a6db8515069907fd5e08b2a
James Majors <noizetoys@yahoo.com>
2023-10-13 13:50:24 -0400

    Initial Commit

---
+++ Feel the Love/Feel_the_LoveApp.swift
@@ -0,0 +1,17 @@
+//
+//  Feel_the_LoveApp.swift
+//  Feel the Love
+//
+//  Created by Apple User on 10/13/23.
+//
+
+import SwiftUI
+
+@main
+struct Feel_the_LoveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
